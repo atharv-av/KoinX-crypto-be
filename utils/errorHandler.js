e6275a54f915bf2c87e7737d44c7524c9b64a536
Atharv Vibhute <atharv.av.codes@gmail.com>
2024-10-11 09:09:29 +0530

    modified for aws conventions

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -3,7 +3,7 @@ const TryCatch = (handler) => {
       try {
         await handler(req, res, next);
       } catch (error) {
-        res.status(500).json({
+        return res.status(500).json({
           success: false,
           message: error.message,
         });
